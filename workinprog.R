11f1330485ef78ffcae2b49ca3fb0bb43e331528
ivanezeigbo <ivyezeigbo@yahoo.com>
2016-10-16 23:22:43 +0200

    First R code of Gary King's CEM test
    Ran a genetic match in place of the CEM test in evaluating the effect of having a daughter on liberality in reproductive right issues.

---
+++ workinprog.R
@@ -0,0 +1,36 @@
+library("Matching")
+library("foreign")
+library("rbounds")
+x <- read.dta("basic.dta")
+#covariates
+x <- na.omit(x) #Leaving  out, specifically, the missing notwot values
+set.seed(14432)
+#x$region <- as.numeric(x$region) #Perhaps their region affects their believes too. So converting the numbers on the region to numeric/integer values for matching
+treat <- x$anygirls
+#Did not pick x$perf because x$female should be more important in my reasoning #x$aauw, x$rtl
+Xi <- cbind(x$female, x$white, x$repub, x$age, x$age ^2, x$srvlng, I(x$srvlng^2), x$statalph, x$rgroup, x$demvote)
+BalanceMat <- cbind(x$female, x$white, x$repub, x$age, x$age ^2, x$srvlng, I(x$srvlng^2), x$statalph, x$rgroup, x$demvote)
+treat <- x$anygirls
+Y <- x$nowtot
+genout <- GenMatch(Tr=treat, X=Xi, BalanceMatrix=BalanceMat, estimand="ATT", M=1,
+                   pop.size=500, max.generations=100, wait.generations=15, cluster = c("localhost", "localhost"))
+mout <- Match(Y=Y, Tr=treat, X=Xi, estimand="ATT", Weight.matrix=genout)
+summary(mout)
+summary(X)
+#Check Balance
+mb <- MatchBalance(treat~x$female + x$white + x$repub + x$age + I(x$age ^2) + x$srvlng + I(x$srvlng^2) + x$statalph + x$rgroup + x$demvote,
+                   match.out=mout, nboots=500)
+#Run linear regression
+#basemod <- lm(nowtot ~x$female + x$white + x$repub + x$age + I(x$age ^2) + x$srvlng + I(x$srvlng^2) + x$statalph + x$rgroup + x$demvote, x)
+par(mfrow = c(1, 2))
+plot(density(rnorm(1000)))
+hist(rnorm(1000))
+plot(density(x$age[treat==1], bw = 2), lwd = 3, col = "red")
+lines(density(x$age[treat==0], bw = 2), lwd = 3, col = "blue")
+plot(density(x$statalph[treat==1], bw = 2), lwd = 3, col = "red")
+lines(density(x$statalph[treat==0], bw = 2), lwd = 3, col = "blue")
+#After
+plot(density(x$age[mout$index.treated], bw = 2), lwd = 3, col = "red")
+lines(density(x$age[mout$index.control], bw = 2), lwd = 3, col = "blue")
+plot(density(x$statalph[mout$index.treated], bw = 2), lwd = 3, col = "red")
+lines(density(x$statalph[mout$index.control], bw = 2), lwd = 3, col = "blue")
