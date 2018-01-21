fab3d0b965f18630ec453e071371d30b8ab21aec
Ivan Chizurum Ezeigbo <ivyezeigbo@yahoo.com>
2018-01-21 21:20:19 +0530

    Update moore.R

--- moore.R
+++ moore.R
@@ -6,7 +6,7 @@ date <- strsplit(as.character(data$testID), '-')
 date <- as.matrix(sapply(date, "[", 2))
 colnames(date) <- "Date"
 Date <- na.omit(cbind(data, date))
-Date$base <- log10(Date$base)
+Date$base <- log2(Date$base)
 plot(Date$Date, Date$base, main = "Semi-Log Plot to Prove Moore's Law", xlab = "Date", ylab = "Base")
 fit <- lm(Date$base ~ Date$Date)
 abline(fit, col = 'red')
